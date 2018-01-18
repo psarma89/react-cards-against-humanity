23497307a1e4283393aa92acae0edc765cb992da
psarma89 <priyam.sarma@gmail.com>
2018-01-18 17:35:14 -0500

    Adds my commits after chris's forks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
       </div>
     );
   }
