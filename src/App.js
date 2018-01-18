138462e0a61460166c68dc5cfc72c522a512d3a0
Chris Johnson <christofer.d.johnson@gmail.com>
2018-01-18 17:30:19 -0500

    Added Login/Logout Functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Main from './components/Main'
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <Main />
       </div>
     );
   }
