302788cea41c2ab1614ad7c8e91c00820587e850
psarma89 <priyam.sarma@gmail.com>
2018-01-18 14:41:00 -0500

    Adds the Navbar component to the front end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
       </div>
     );
   }
